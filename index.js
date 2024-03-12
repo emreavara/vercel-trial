8c321e0ac033922b9b14223f71dfaf8460660127
Emre Avara <emre_avara@hotmail.com>
2024-03-12 23:38:17 +0100

    Add simple example

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, (req, res) => {
+  console.log("App is running on port: ", PORT);
+});
